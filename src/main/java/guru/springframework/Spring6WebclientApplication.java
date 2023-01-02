b10c945cf5e8e5715964bc0cc58f99f3034103c5
John Thompson <john@springframework.guru>
2023-01-02 15:13:03 -0500

    initial project add

---
+++ src/main/java/guru/springframework/Spring6WebclientApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6WebclientApplication.class, args);
+    }
+
+}
